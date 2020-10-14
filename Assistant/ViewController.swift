20b71097a93b8c91c618a00a8525ca87f6ddeecd
Jeffrey <Jeffrey@simpleyapp.com>
2020-10-14 15:17:35 -0400

    Initial Commit

---
+++ Assistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assistant
+//
+//  Created by Jeffrey  on 10/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
